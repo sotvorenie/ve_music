6057249453120c66e206a589f1bca4aff2b05989
vitalik abrosimov <vitalikabrosimov00@gmail.com>
2026-09-09 13:30:28 +0300

    2 запроса для artist объединены в 1

--- src/api/artist/artist.ts
+++ src/api/artist/artist.ts
@@ -2,10 +2,6 @@ import {ArtistsList} from "@/types/artist.ts";
 
 import {apiGet} from "@api/index.ts";
 
-export const apiGetAllArtists = async(page: number = 1, limit: number = 21): Promise<ArtistsList> => {
-    return apiGet(`/artist/all?page=${page}&limit=${limit}`)
-}
-
-export const apiSearchArtist = async (name: string, page: number = 1, limit: number = 21): Promise<ArtistsList> => {
-    return apiGet(`/artist/search?name=${name}&page=${page}&limit=${limit}`)
+export const apiGetArtists = async(name: string = '', page: number = 1, limit: number = 21): Promise<ArtistsList> => {
+    return apiGet(`/artist/list?name=${name}&page=${page}&limit=${limit}`)
 }
