44e744868b67066c7bc412ffcec5ae7c09114960
vitalik abrosimov <vitalikabrosimov00@gmail.com>
2026-09-11 00:55:53 +0300

    анимация пластинки на проигрываемой музыке

--- src/components/block/ListMusic.vue
+++ src/components/block/ListMusic.vue
@@ -11,6 +11,8 @@ import Empty from "@ui/empty/Empty.vue";
 import useItemsStore from "@store/useItemsStore.ts";
 const itemsStore = useItemsStore();
 import useAudioStore from "@store/useAudioStore.ts";
+import Icon from "@ui/Icon.vue";
+import MusicRecordIcon from "@icons/MusicRecordIcon.vue";
 const audioStore = useAudioStore();
 
 const observerLi = ref<HTMLLIElement | null>(null)
@@ -63,15 +65,27 @@ watchEffect((onCleanup) => {
   >
     <li v-for="(item, index) in itemsStore.musicList?.music"
         :key="item.id"
-        class="list__item flex cursor-pointer flex-align-center"
+        class="list__item flex cursor-pointer flex-align-center position-relative"
         :class="{'is-active': item.id === audioStore.activeTrack.id}"
         @click="audioStore.updateMusic(item.id)"
         :ref="(el) => { if (index === itemsStore.musicList!.music.length - 2) observerLi = el as HTMLLIElement }"
     >
-      <div class="list__left img-container">
+      <div class="list__left img-container z-100">
         <img v-if="item?.previewUrl" :src="`${BASE_URL}${item?.previewUrl}`" :alt="item?.name">
       </div>
-      <div class="list__right overflow-hidden">
+
+      <Transition name="record">
+        <Icon v-if="item.id === audioStore.activeTrack.id"
+              :name="MusicRecordIcon"
+              :size="70"
+              recolor
+              class="list__item-record position-absolute z-10"
+        />
+      </Transition>
+
+      <div class="list__right overflow-hidden"
+           :class="item.id === audioStore.activeTrack.id && 'is-active'"
+      >
         <p class="list__name text-w500 text-ellipsis">{{item?.name}}</p>
         <p class="list__artist text-ellipsis">{{showArtists(item?.artists)}}</p>
         <p class="list__duration">{{formatTime(item.duration)}}</p>
